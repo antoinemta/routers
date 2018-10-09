4297f6a5d829ef1c7241e2b590d3297f7a5a548f
antoinemta <antoine_mta@hotmail.fr>
2018-10-09 13:37:28 +0200

    I added a routes.

---
+++ src/History.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Home extends Component {
+  render() {
+    return <div className="App">Notre histoire</div>;
+  }
+}
+
+export default Home;
